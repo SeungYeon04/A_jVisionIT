eab02103b1a1512ad7ecab628e937a859b5a8031
b-hyoung <youqlrqod@gmail.com>
2025-07-03 22:27:06 +0900

    add header & login

---
+++ b-hyh/header.js
@@ -0,0 +1,36 @@
+function openLoginModal() {
+  fetch('./login.html')
+    .then(res => res.text())
+    .then(html => {
+      const backdrop = document.getElementById('login-backdrop');
+      const container = document.getElementById('login-modal-container');
+
+      container.innerHTML = html;
+      backdrop.style.display = 'block';
+      container.style.display = 'block';
+
+      backdrop.onclick = () => {
+        container.innerHTML = '';
+        backdrop.style.display = 'none';
+        container.style.display = 'none';
+      };
+
+      import('./login.js').then(module => {
+        module.initLoginModal(); // ✅ 여기서 매번 초기화
+      });
+    });
+}
+
+    function toggleMenu() {
+      const modal = document.getElementById("modal-menu");
+      const backdrop = document.getElementById("menu-backdrop");
+      const isOpen = modal.classList.contains("show");
+
+      if (isOpen) {
+        modal.classList.remove("show");
+        backdrop.style.display = "none";
+      } else {
+        modal.classList.add("show");
+        backdrop.style.display = "block";
+      }
+    }

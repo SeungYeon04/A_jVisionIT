a26a11d15b8b40b6870c6bdd839e0d505c317931
b-hyoung <youqlrqod@gmail.com>
2025-07-05 13:20:45 +0900

    추가 네비게이션바

--- js/initHeader.js
+++ js/initHeader.js
@@ -1,8 +1,10 @@
 fetch('/module/header.html')
-    .then(res => res.text())
-    .then(html => {
-        document.getElementById('header-container').innerHTML = html;
-    });
-const script = document.createElement('script');
-script.src = '/js/header.js';
-document.body.appendChild(script);
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('header-container').innerHTML = html;
+
+    // ✅ header가 삽입된 이후에 script 삽입
+    const script = document.createElement('script');
+    script.src = '/js/header.js';
+    document.body.appendChild(script);
+  });
